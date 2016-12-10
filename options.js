080f42a1cd2015aa5e539d72012a7795298a8aa8
Daniel B <daniel.betz@daniel-betz.com>
2016-12-10 16:33:47 +0100

    Native styling (tested on macOS)

--- options.js
+++ options.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-let inputField = document.getElementById("delayInput");
+const inputField = document.getElementById("delayInput");
 const settingsKey = "removalDelayInMinutes";
 
 inputField.addEventListener("change", () => {
